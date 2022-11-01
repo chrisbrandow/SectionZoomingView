91650ce1fd036dfab5a08b70cf5725a6e81a208c
Doug Boutwell <dboutwell@opentable.com>
2022-11-01 16:30:36 -0700

    Add per diner / course accessors for cart

--- SectionZoomingView/CartView/CartViewModel.swift
+++ SectionZoomingView/CartView/CartViewModel.swift
@@ -8,3 +8,34 @@ class CartViewModel: ObservableObject {
         self.cart = cart
     }
 }
+
+extension Cart {
+    func itemsByDinerId() -> [Item.DinerID: [Item]] {
+        var result: [Item.DinerID: [Item]] = [:]
+        self.items.forEach { item in
+            item.diners.forEach { diner in
+                var dinerItems = result[diner] ?? []
+                dinerItems.append(item)
+                result[diner] = dinerItems
+            }
+        }
+        return result
+    }
+
+    /// All items ordered by their courses.
+    ///
+    /// Empty courses will be compacted - for instance, an order whose items only
+    /// belong to courses 0, 1, and 4 will produce an array of length 3
+    func itemsByCourse() -> [[Item]] {
+        var courses: [Int: [Item]] = [:]
+        self.items.forEach { item in
+            var courseItems = courses[item.course] ?? []
+            courseItems.append(item)
+            courses[item.course] = courseItems
+        }
+        return courses.keys
+            .sorted()
+            .map { courses[$0] }
+            .compactMap { $0 }
+    }
+}
