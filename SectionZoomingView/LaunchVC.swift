f2de3393a2d33325d633227626f5de0ea1ad2c28
chrisbrandow <chrisbrandow@gmail.com>
2022-11-01 22:10:01 -0700

    * updated app icon * updated launch screen

--- SectionZoomingView/LaunchVC.swift
+++ SectionZoomingView/LaunchVC.swift
@@ -12,42 +12,36 @@ class LaunchVC: UIViewController {
 
     @IBOutlet var stackView: UIStackView?
 
+    @IBOutlet weak var mockStackView: UIStackView!
     // to make it easier to integrate into OpenTable, i should move teh datasource
     // to MenuParentViewController, then "load" it from there, based on the name
     var selectedExample = TakeoutDataSource.Example.mint_11_123
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        for example in TakeoutDataSource.Example.allCases {
-            guard example != .betterZoom else { continue }
-            let button = UIButton(type: .custom)
-            button.setTitle(example.displayName, for: .normal)
-            button.setTitleColor(.darkGray, for: .normal)
-            button.backgroundColor = .lightGray
-            button.layer.cornerRadius = Layout.cornerRadius
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        for (button, example) in zip(self.mockStackView.arrangedSubviews.compactMap({ $0 as? UIButton}), TakeoutDataSource.Example.allCases) {
             button.addAction(UIAction(handler: { [weak self] _ in
                 guard let self = self else { return }
                 self.selectedExample = example
                 self.performSegue(withIdentifier: "showZoomable", sender: self)
 
             }), for: .touchUpInside)
-            self.stackView?.addArrangedSubview(button)
+
         }
 
-        let button = UIButton(type: .custom)
-        button.setTitle("just show the better zoom", for: .normal)
-        button.setTitleColor(.lightGray, for: .normal)
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = Layout.cornerRadius
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.addAction(UIAction(handler: { [weak self] _ in
-            guard let self = self else { return }
-            self.selectedExample = .betterZoom
-            self.performSegue(withIdentifier: "showBetterZoom", sender: self)
-
-        }), for: .touchUpInside)
-        self.stackView?.addArrangedSubview(button)
+//        let button = UIButton(type: .custom)
+//        button.setTitle("just show the better zoom", for: .normal)
+//        button.setTitleColor(.lightGray, for: .normal)
+//        button.backgroundColor = .systemBlue
+//        button.layer.cornerRadius = Layout.cornerRadius
+//        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+//        button.addAction(UIAction(handler: { [weak self] _ in
+//            guard let self = self else { return }
+//            self.selectedExample = .betterZoom
+//            self.performSegue(withIdentifier: "showBetterZoom", sender: self)
+//
+//        }), for: .touchUpInside)
+//        self.stackView?.addArrangedSubview(button)
     }
 
     override func viewDidAppear(_ animated: Bool) {
