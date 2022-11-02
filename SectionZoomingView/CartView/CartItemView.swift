43205f2e31fa43cc5b85020147698daa60ba1152
Doug Boutwell <dboutwell@opentable.com>
2022-11-02 15:57:50 -0700

    Tweak cart item view style

--- SectionZoomingView/CartView/CartItemView.swift
+++ SectionZoomingView/CartView/CartItemView.swift
@@ -3,19 +3,24 @@ import SwiftUI
 struct CartItemView: View {
     @State var item: Cart.Item
 
+    @State var shouldShowDescription: Bool = false
+
+    var lineLimit: Int = 1
+
     var body: some View {
         VStack(alignment: .leading, spacing: .otk_mediumSpacing) {
             HStack(alignment: .top, spacing: .otk_mediumSpacing) {
                 Text(item.menuItem.name)
-                    .otk_configureBodyText(fontSize: 14, weight: .bold)
+                    .otk_configureBodyText(fontSize: 16, weight: .semibold)
+                    .lineLimit(lineLimit)
                     .layoutPriority(1)
                 Spacer()
                 Text(item.menuItem.price.formattedDescription ?? "")
-                    .otk_configureBodyText(fontSize: 14)
+                    .otk_configureBodyText(fontSize: 16)
                     .foregroundColor(.ash_dark)
                     .fixedSize(horizontal: true, vertical: false)
             }
-            if let description = item.menuItem.itemDescription {
+            if let description = item.menuItem.itemDescription, shouldShowDescription {
                 Text(description)
                     .otk_configureBodyText(fontSize: 12)
                     .lineLimit(2)
