48a90c0deffaa600411dfe60b6dd2fb476d6c0b0
ryosuke <85515369+ryosuke-ot@users.noreply.github.com>
2022-11-02 19:17:48 -0700

    feat(tag-bar): show tags in menu

--- SectionZoomingView/ZoomableMenu/CoreMenuView.swift
+++ SectionZoomingView/ZoomableMenu/CoreMenuView.swift
@@ -7,6 +7,15 @@
 
 import UIKit
 
+enum MenuTag: String, Equatable, CaseIterable {
+    case none = "None"
+    case vegetarian = "Vegetarian"
+    case glutenFree = "Gluten Free"
+    case vegan = "Vegan"
+    case raw = "Raw"
+    case dairy = "Dairy"
+}
+
 class EntryView: UIView {
     var item: MenuItem?
 
@@ -16,6 +25,8 @@ class EntryView: UIView {
     var priceLabel = UILabel()
     var button = UIButton()
     var badge = UIView()
+    var tagStackView: UIStackView?
+    
     override var description: String {
         return self.frame.debugDescription// self.item?.name ?? " no item"
     }
@@ -159,8 +170,19 @@ class PlaceholderMenuView: UIView {
         view.addSubview(button)
         var vFrame = view.frame
         let addedHeight = description.frame.height > 0 ? description.frame.height + 4 + 8 : 8
-        
-        vFrame.size.height = titleLabel.frame.height + addedHeight
+
+        if let tagStackView = makeTagStackViewIfNecessary(attributes: entry.attributes) {
+            tagStackView.frame = view.isNarrowCell ? CGRect.zero : CGRect(x: 8, y: defaultTextHeight + 4 + topSpacing + description.frame.height + 8, width: columnWidth - 16, height: 24)
+            view.addSubview(tagStackView)
+
+            vFrame.size.height = titleLabel.frame.height + addedHeight + 32
+            view.tagStackView = tagStackView
+        }
+        else {
+            vFrame.size.height = titleLabel.frame.height + addedHeight
+            view.tagStackView = nil
+        }
+
         button.frame = vFrame
         let cornerRadius: CGFloat = Layout.menuItemMargin
         let shadowView = UIView(frame: vFrame)
@@ -191,6 +213,47 @@ class PlaceholderMenuView: UIView {
         view.badge = badge
         return shadowView
     }
+
+    private static func makeTagStackViewIfNecessary(attributes: [String]) -> UIStackView? {
+        guard !attributes.isEmpty else {
+            return nil
+        }
+
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .leading
+        stackView.spacing = 8
+
+        let tags = attributes.compactMap { MenuTag(rawValue: $0) }
+        makeSubviews(with: tags).forEach { stackView.addArrangedSubview($0) }
+
+        let spacerView = UIView()
+        spacerView.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        stackView.addArrangedSubview(spacerView)
+
+        return stackView
+    }
+
+    private static func makeSubviews(with tags: [MenuTag]) -> [UIView] {
+        var views = [UIView]()
+
+        tags.forEach { tag in
+            let label = UILabel()
+            label.backgroundColor = .otk_greenLightest
+            label.text = tag.rawValue
+            if tag == .raw {
+                label.text = "Contains raw meat"
+            }
+            label.textColor = .otk_greenDark
+            label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+            label.layer.cornerRadius = 4
+
+            views.append(label)
+        }
+
+        return views
+    }
 }
 
 
@@ -214,11 +277,13 @@ extension EntryView {
             self.titleLabel.layer.opacity = 1.0
             self.descriptionLabel.layer.opacity = 1.0
             self.priceLabel.layer.opacity = 1.0
+            tagStackView?.layer.opacity = 1.0
         case .compressed:
             self.bigTitleLabel.layer.opacity = 1.0
             self.titleLabel.layer.opacity = 0.0
             self.descriptionLabel.layer.opacity = 0.0
             self.priceLabel.layer.opacity = 0.0
+            tagStackView?.layer.opacity = 0.0
         }
     }
     func configure(style: EntryViewStyle) {
